bcfc99ecd54612c664b30dbe16a0a69df8db87a3
junkaM <hei.mikanto.3@gmail.com>
2025-02-10 12:22:03 +0100

    feat: add helloWorld controller

---
+++ src/main/java/com/example/demo/endpoint/rest/controller/health/helloWorldController.java
@@ -0,0 +1,13 @@
+package com.example.demo.endpoint.rest.controller.health;
+
+import java.util.Map;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class helloWorldController {
+  @GetMapping("/hello")
+  public Map<String, String> hello() {
+    return Map.of("Message", "Hello World");
+  }
+}
